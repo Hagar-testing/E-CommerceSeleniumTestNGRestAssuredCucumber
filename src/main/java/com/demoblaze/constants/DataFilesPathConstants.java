b734629bd561639de1c9a3e90d6d66df492a79c0
Hager <hajer.ibr@gmail.com>
2024-05-13 19:38:06 +0300

    Create DataUtils

---
+++ src/main/java/com/demoblaze/constants/DataFilesPathConstants.java
@@ -0,0 +1,9 @@
+package com.demoblaze.constants;
+
+public class DataFilesPathConstants {
+    public static final String DATA_FILE_PATH = "//src//test//resources//testDataFiles//";
+    public static String projectPath = System.getProperty("user.dir");
+    public static final String REGISTER_DATA_FILE_PATH = "RegisterUser.json";
+    public static final String REGISTER_USER_DATA = "registerUserData";
+
+}
---
+++ src/main/java/com/demoblaze/utils/DataUtils.java
@@ -0,0 +1,36 @@
+package com.demoblaze.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.demoblaze.constants.DataFilesPathConstants.DATA_FILE_PATH;
+import static com.demoblaze.constants.DataFilesPathConstants.projectPath;
+
+
+
+public class DataUtils {
+
+
+    public static Object[][] readAndProvideJsonData(String fileName) throws IOException {
+        String jsonContent = FileUtils.readFileToString(new File(projectPath + DATA_FILE_PATH + fileName), StandardCharsets.UTF_8);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<HashMap<String, String>> map = mapper.readValue(jsonContent, new TypeReference<>() {});
+
+        Object[][] data = new Object[map.size()][1];
+
+        for (int i = 0; i < map.size(); i++) {
+            data[i][0] = map.get(i);
+        }
+
+        return data;
+    }
+}
+
