7c6e84aea598e9ea4e91bd41f2c2581fb6d7c99e
Hagar <hagar.testing@gmail.com>
2024-05-13 22:25:19 +0300

    Add after method

--- src/test/java/tests/SignupTest.java
+++ src/test/java/tests/SignupTest.java
@@ -9,6 +9,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.HashMap;
@@ -47,5 +48,9 @@ public class SignupTest {
     }
 
 
+    @AfterMethod
+    public void afterMethod(){
+        driver.get().quit();
+    }
 
 }
