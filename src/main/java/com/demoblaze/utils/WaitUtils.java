c15d849f865903d37d16f800b06a1291dbe0f03c
Hager <hajer.ibr@gmail.com>
2024-05-13 19:31:22 +0300

    Create WaitUtils to handle explicit wait

---
+++ src/main/java/com/demoblaze/utils/WaitUtils.java
@@ -0,0 +1,38 @@
+package com.demoblaze.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+    private static final Duration DEFAULT_WAIT_DURATION = Duration.ofSeconds(10);
+
+
+    private static WebDriverWait getWebDriverWait(WebDriver driver) {
+        return new WebDriverWait(driver, DEFAULT_WAIT_DURATION);
+    }
+
+    public void waitForElementToBeClickable(WebDriver driver, By locator) {
+        getWebDriverWait(driver).until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public static WebElement waitForVisibilityOfElement(WebDriver driver, By locator) {
+        return getWebDriverWait(driver).until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public static void waitForInvisibilityOfElement(WebDriver driver, WebElement element) {
+        getWebDriverWait(driver).until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public static WebElement waitForPresenceOfElement(WebDriver driver, By locator) {
+        return getWebDriverWait(driver).until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public static void waitForTextToBePresentInElement(WebDriver driver, By locator, String text) {
+        getWebDriverWait(driver).until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+    }
+}
