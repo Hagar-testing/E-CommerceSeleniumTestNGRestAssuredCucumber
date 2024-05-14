cab7698c5d860b6484c8670da576014b124bf6b9
Hagar <hagar.testing@gmail.com>
2024-05-14 21:00:58 +0300

    Refactor product page name

--- src/main/java/com/demoblaze/pages/ProductDetailsPage.java
+++ src/main/java/com/demoblaze/pages/ProductDetailsPage.java
@@ -1,40 +1,38 @@
 package com.demoblaze.pages;
 
-import com.demoblaze.utils.BrowserActions;
 import com.demoblaze.utils.ElementActions;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 
 import static com.demoblaze.utils.BrowserActions.dismissAlertDialog;
 import static com.demoblaze.utils.BrowserActions.getAlertMessage;
 import static org.testng.AssertJUnit.assertEquals;
 
-public class ProductPage {
+public class ProductDetailsPage {
     private final WebDriver driver;
     private final ElementActions elementActions;
 
     final By addToCart_button = By.cssSelector("a[onclick*='addToCart']");
 
-    public ProductPage(WebDriver driver){
+    public ProductDetailsPage(WebDriver driver){
         this.driver = driver;
         this.elementActions = new ElementActions(driver);
     }
 
-    public ProductPage clickOnAddToCartButton() throws InterruptedException {
+    public ProductDetailsPage clickOnAddToCartButton() throws InterruptedException {
         elementActions.click(addToCart_button);
         return this;
     }
 
-    public ProductPage hideAlertDialog(){
+    public ProductDetailsPage hideAlertDialog(){
        dismissAlertDialog(driver);
         return this;
     }
 
     //region Validations
     @Step("Validate that {message} of add product to cart is shown successfully!")
-    public ProductPage validateOnSuccessMessageOfAddProductToCart(String message){
+    public ProductDetailsPage validateOnSuccessMessageOfAddProductToCart(String message){
         assertEquals(getAlertMessage(driver),message);
         return this;
     }
