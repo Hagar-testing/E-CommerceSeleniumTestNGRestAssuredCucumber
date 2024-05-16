22f4aedb344e4d1f70a69927dbf8cefeef6f6d55
Hagar <hagar.testing@gmail.com>
2024-05-16 15:40:59 +0300

    Create PlaceOrder step defintion file

---
+++ src/test/java/steps/PlaceOrder.java
@@ -0,0 +1,140 @@
+package steps;
+
+import com.demoblaze.api.ApisAuthentications;
+import com.demoblaze.factory.DriverFactory;
+import com.demoblaze.pages.*;
+import com.demoblaze.utils.JsonUtils;
+import com.google.gson.JsonObject;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Objects;
+
+import static com.demoblaze.data.DataFilesPathConstants.CHECKOUT_DATA_FILE_PATH;
+import static com.demoblaze.utils.JsonUtils.getTestData;
+
+public class PlaceOrder {
+
+    private final JsonObject testData;
+    private final String timeStamp = String.valueOf(System.currentTimeMillis());
+    WebDriver driver;
+
+    public PlaceOrder() {
+        testData = JsonUtils.parseJsonFile(CHECKOUT_DATA_FILE_PATH);
+        driver = new DriverFactory().initializeDriver();
+
+
+    }
+
+
+    @Given("I am a registered user on DemoBlaze")
+    public void registerUser() {
+        new ApisAuthentications()
+                .registerUser(
+                        getTestData(testData, "user.name") + timeStamp,
+                        Objects.requireNonNull(getTestData(testData, "user.password")));
+    }
+
+    @When("I log in to the website with valid credentials")
+    public void loginToWebsite() {
+        new HomePage(driver).load();
+        new Header(driver).clickOnLoginButton();
+        new LoginPage(driver).loginUser(getTestData(testData,"user.name") + timeStamp, getTestData(testData,"user.password"));
+    }
+
+    @Then("I validate that the account is opened successfully")
+    public void validateOnAccountIsOpenedSuccessfully() {
+        new Header(driver).validateOnAccountIsOpenedSuccessfully();
+    }
+
+    @And("I select a category")
+    public void chooseCategory() {
+        new HomePage(driver).selectCategory(getTestData(testData, "category.name"));
+    }
+
+    @When("I add the first product to my cart")
+    public void addFirstProductToCart() {
+        new HomePage(driver)
+                .selectProduct(1, getTestData(testData, "category.products.first_product.title"));
+        new ProductDetailsPage(driver)
+                .clickOnAddToCartButton();
+    }
+
+    @Then("I should see a success message indicating the product is added")
+    public void validateFirstProductAdded() {
+        new ProductDetailsPage(driver)
+                .validateOnSuccessMessageOfAddProductToCart(getTestData(testData, "messages.add_product_to_cart"))
+                .hideAlertDialog();
+    }
+
+    @When("I add the second product to my cart")
+    public void addSecondProductToCart() {
+        new ProductDetailsPage(driver)
+                .navigateBack()
+                .navigateBack();
+        new HomePage(driver)
+                .selectProduct(2, getTestData(testData, "category.products.second_product.title"));
+        new ProductDetailsPage(driver)
+                .clickOnAddToCartButton();
+    }
+
+    @Then("I should see a success message indicating the second product is added")
+    public void validateSecondProductAdded() {
+        new ProductDetailsPage(driver)
+                .validateOnSuccessMessageOfAddProductToCart(getTestData(testData, "messages.add_product_to_cart"))
+                .hideAlertDialog();
+    }
+
+    @When("I view my cart")
+    public void viewCart() {
+        new Header(driver).clickOnCartButton();
+    }
+
+    @Then("I should see both selected products listed in the cart")
+    public void validateProductsInCart() {
+        new CartPage(driver)
+                .validateOnItemAddedInCart(getTestData(testData, "category.products.first_product.title"))
+                .validateOnItemAddedInCart(getTestData(testData, "category.products.second_product.title"));
+    }
+
+    @And("the total price should be calculated correctly based on product prices")
+    public void validateTotalPrice() {
+        new CartPage(driver)
+                .validateOnTotalProductPrice(getTestData(testData, "cart.total_price"));
+    }
+
+    @When("I proceed to place order")
+    public void navigateToPlaceOrder() {
+        new CartPage(driver).clickOnPlaceOrderButton();
+
+    }
+
+    @Then("I should see the same total price on the place order page")
+    public void validateOnTotalPriceInPlaceOrderPage() {
+        new PlaceOrderPage(driver)
+                .validateOnTotalPriceInPlaceOrder(getTestData(testData, "cart.total_price"));
+    }
+
+    @When("I fill in my order details and submit")
+    public void fillOrderData() {
+        new PlaceOrderPage(driver)
+                .fillOrderInformationAndPurchase(
+                getTestData(testData, "place_order_data.name"),
+                getTestData(testData, "place_order_data.country"),
+                getTestData(testData, "place_order_data.city"),
+                getTestData(testData, "place_order_data.month"),
+                getTestData(testData, "place_order_data.card"),
+                getTestData(testData, "place_order_data.year"));
+    }
+
+    @Then("I should see a success message confirming my order")
+    public void validateOnPlaceOrderSuccessMessage() {
+        new PlaceOrderPage(driver)
+                .validateOnSuccessMessageOfPurchaseOrder(getTestData(testData, "messages.place_order"));
+    }
+
+}
+
