0b900d66e86e587f5c5817860fd1737a8474919c
Hagar <hagar.testing@gmail.com>
2024-05-17 00:25:39 +0300

    Update ExtentReport to handle null values

--- src/main/java/com/demoblaze/utils/ExtentReport.java
+++ src/main/java/com/demoblaze/utils/ExtentReport.java
@@ -23,10 +23,8 @@ public class ExtentReport {
     }
 
     public static void createTest(String testCaseName) {
-        if(threadLocal.get() != null) {
-            test = report.createTest(testCaseName);
-            threadLocal.set(test);
-        }
+        test = report.createTest(testCaseName);
+        threadLocal.set(test);
     }
 
     public static void removeTest(String testCaseName) {
@@ -40,47 +38,47 @@ public class ExtentReport {
     }
 
     public static void info(Markup m) {
-        if(threadLocal.get() != null) threadLocal.get().info(m);
+        if(test != null) threadLocal.get().info(m);
     }
 
     public static void pass(String message) {
-        if(threadLocal.get() != null) threadLocal.get().pass(message);
+        if(test != null) threadLocal.get().pass(message);
     }
 
     public static void pass(Markup m) {
-        if(threadLocal.get() != null) threadLocal.get().pass(m);
+        if(test != null) threadLocal.get().pass(m);
     }
 
     public static void fail(String message) {
-        if(threadLocal.get() != null) threadLocal.get().fail(message);
+        if(test != null) threadLocal.get().fail(message);
     }
 
     public static void fail(Markup m) {
-        if(threadLocal.get() != null) threadLocal.get().fail(m);
+        if(test != null) threadLocal.get().fail(m);
     }
 
     public static void fail(Throwable t) {
-        if(threadLocal.get() != null) threadLocal.get().fail(t);
+        if(test != null) threadLocal.get().fail(t);
     }
 
     public static void fail(Media media) {
-        if(threadLocal.get() != null) threadLocal.get().fail(media);
+        if(test != null) threadLocal.get().fail(media);
     }
 
     public static void skip(String message) {
-        if(threadLocal.get() != null) threadLocal.get().skip(message);
+        if(test != null) threadLocal.get().skip(message);
     }
 
     public static void skip(Markup m) {
-        if(threadLocal.get() != null) threadLocal.get().skip(m);
+        if(test != null) threadLocal.get().skip(m);
     }
 
     public static void skip(Throwable t) {
-        if(threadLocal.get() != null) threadLocal.get().skip(t);
+        if(test != null) threadLocal.get().skip(t);
     }
 
     public static void flushReports() {
-        if(threadLocal.get() != null) report.flush();
+        if(test != null) report.flush();
     }
 
 }
